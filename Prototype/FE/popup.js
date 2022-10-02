8cda7c84c4fed12c225388e67d28dff0878232a3
Ho-hoho <kennyya@naver.com>
2022-10-02 15:41:49 +0900

    removed background color change

--- Prototype/FE/popup.js
+++ Prototype/FE/popup.js
@@ -1,9 +1,5 @@
 let changeColor = document.getElementById("changeColor");
 
-chrome.storage.sync.get("color", ({ color }) => {
-  changeColor.style.backgroundColor = color;
-});
-
 // When the button is clicked, inject setPageBackgroundColor into current page
 changeColor.addEventListener("click", async () => {
     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
@@ -12,15 +8,6 @@ changeColor.addEventListener("click", async () => {
       func: setUrlColor,
     });
   });
-  
-  // The body of this function will be executed as a content script inside the
-  // current page
-  function setPageBackgroundColor() {
-    chrome.storage.sync.get("color", ({ color }) => {
-        document.body.style.backgroundColor = color;
-
-    });
-  }
 
   function setUrlColor() {
     document.querySelectorAll("li").forEach(item => {
