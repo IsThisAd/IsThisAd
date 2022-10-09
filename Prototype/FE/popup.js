8db1e74bc77c3e549ce96bdbe7fbe91cc2becd2e
Ho-hoho <kennyya@naver.com>
2022-10-09 16:09:32 +0900

    1009_1609

--- Prototype/FE/popup.js
+++ Prototype/FE/popup.js
@@ -5,19 +5,7 @@ changeColor.addEventListener("click", async () => {
     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
     chrome.scripting.executeScript({
       target: { tabId: tab.id },
-      func: setUrlColor,
+      //func: setUrlColor,
+      files : ["scraping.js"],
     });
   });
-
-  function setUrlColor() {
-    var urlRegex = /(https?:\/\/[^ ]*)/
-    document.querySelectorAll("li").forEach(item => {
-      //console.log(item.innerHTML);
-      //console.log("\n");
-      var url = item.innerHTML.match(urlRegex)
-      url = item.innerHTML.match(urlRegex) ? item.innerHTML.match(urlRegex)[1] : ''
-      if(url.length > 0)
-        console.log(url);
-    })
-    
-  }
