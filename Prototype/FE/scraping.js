4114d446c1384b3ff6ba22e1fd0409f19f08222b
Ho-hoho <kennyya@naver.com>
2022-10-09 17:50:29 +0900

    changed color

--- Prototype/FE/scraping.js
+++ Prototype/FE/scraping.js
@@ -1,17 +1,21 @@
 
 function setUrlColor() {
     var urlRegex = /(https?:\/\/blog[^ "]*)/
-      document.querySelectorAll("li").forEach(item => {
+    var flag = false;  
+    document.querySelectorAll("li").forEach(item => {
         var url = item.innerHTML.match(urlRegex)
         url = item.innerHTML.match(urlRegex) ? item.innerHTML.match(urlRegex)[1] : ''
         if(url.length > 0){
-         scrapUrl(url,item);
+         scrapUrl(url,item,flag);
+         flag = !flag;
         }
       })
   }
-  
-  function scrapUrl(url, item){
-    console.log(item);
+  function scrapUrl(url, item,flag){
+    if(flag)
+        item.style.cssText = "background-color: #ccffcc";
+    else
+        item.style.cssText = "background-color: #ffcccc"
   }
   
   setUrlColor();
