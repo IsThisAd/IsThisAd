6ef3eb1d6105e32383494dcb462ddb983817f817
Ho-hoho <kennyya@naver.com>
2022-10-10 11:12:06 +0900

    Combined with YKY

--- Prototype/FE/scraping.js
+++ Prototype/FE/scraping.js
@@ -1,21 +1,49 @@
 
 function setUrlColor() {
     var urlRegex = /(https?:\/\/blog[^ "]*)/
-    var flag = false;  
+    var flag = false;
     document.querySelectorAll("li").forEach(item => {
         var url = item.innerHTML.match(urlRegex)
         url = item.innerHTML.match(urlRegex) ? item.innerHTML.match(urlRegex)[1] : ''
-        if(url.length > 0){
-         scrapUrl(url,item,flag);
-         flag = !flag;
+        if (url.length > 0) {
+            if (!url.includes("MyBlog")) {
+                scrapUrl(url, item);
+                changeBlockColor(url, item, flag);
+            }
+            flag = !flag;
         }
-      })
-  }
-  function scrapUrl(url, item,flag){
-    if(flag)
+    })
+}
+
+function changeBlockColor(url, item, flag) {
+    if (flag)
         item.style.cssText = "background-color: #ccffcc";
     else
         item.style.cssText = "background-color: #ffcccc"
-  }
-  
-  setUrlColor();
+}
+
+function scrapUrl(url, item) {
+    if (url.includes("MyBlog")) {
+        return
+    }
+
+    newURL = makeNewURL(url)
+
+    const request = new XMLHttpRequest()
+    request.open('GET', newURL, true)
+    request.onload = function () {
+        //console.log(request.responseText.match(/\<div id="post-view.*\<\/div>/));
+        var parser = new DOMParser()
+        var doc = parser.parseFromString(request.responseText, "text/html")
+        console.log(doc)
+    }
+    request.send()
+}
+
+function makeNewURL(url) {
+    strs = url.split('/')
+    var newURL = "https://blog.naver.com/PostView.nhn?blogId=" + strs[3] + "&logNo=" + strs[4]
+
+    return newURL
+}
+setUrlColor();
