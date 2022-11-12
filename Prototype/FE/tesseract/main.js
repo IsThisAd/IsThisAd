15a6055f51caac42157f25937c2667c2c669253b
Ho-hoho <kennyya@naver.com>
2022-11-12 16:39:54 +0900

    scraping url and text

--- Prototype/FE/tesseract/main.js
+++ Prototype/FE/tesseract/main.js
@@ -4,9 +4,9 @@ const doOCR = async () => {
   
     const { createWorker } = Tesseract;
     const worker = createWorker({
-      //workerPath: chrome.runtime.getURL('js/worker.min.js'),
-      //langPath: chrome.runtime.getURL('.'),
-      //corePath: chrome.runtime.getURL('js/tesseract-core.wasm.js'),
+      workerPath: chrome.runtime.getURL('js/worker.min.js'),
+      langPath: chrome.runtime.getURL('.'),
+      corePath: chrome.runtime.getURL('js/tesseract-core.wasm.js'),
     });
     
     await worker.load();
@@ -14,16 +14,20 @@ const doOCR = async () => {
     await worker.initialize('eng');
     const { data: { text } } = await worker.recognize(image);
     console.log(text);
-    //result.innerHTML = `<p>OCR Result:</p><p>${text}</p>`;
+    result.innerHTML = `<p>OCR Result:</p><p>${text}</p>`;
     await worker.terminate();
   }
-  
-  const startBtn = document.getElementById('changeColor');
-  startBtn.onclick = doOCR;
-  
+  const doTest = (text) =>{
+    console.log(text);
+  }
+
+  //const startBtn = document.getElementById('changeColor');
+  //startBtn.onclick = doOCR;
+  /*
   changeColor.addEventListener("click", async () => {
-    //let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
     
     doOCR()
   });
+  */
   
