4ce972eb0f6593c60eaf090aaffe583dfff8200d
uyt8989 <kyungyun98@naver.com>
2022-11-12 16:45:42 +0900

    change doOCR

--- Prototype/FE/tesseract/main.js
+++ Prototype/FE/tesseract/main.js
@@ -1,20 +1,28 @@
-async function doOCR(images) {
-  const image = document.getElementById('image');
-  
+async function doOCR(image_urls) {
+
   const worker = new Tesseract.createWorker({
     "workerBlobURL": false,
     "workerPath": chrome.runtime.getURL("tesseract/worker.min.js"),
     "corePath": chrome.runtime.getURL("tesseract/tesseract-core.wasm.js"),
     "langPath": "https://raw.githubusercontent.com/naptha/tessdata/gh-pages/4.0.0_fast"
   });
-    
+
   await worker.load();
   await worker.loadLanguage('kor');
   await worker.initialize('kor');
-  const data = await worker.recognize(image);
-  console.log(data);
+
+  ocr_text = []
+
+  for(var url in image_urls) {
+    const data = await worker.recognize(url);
+    orc_text.apppend(data)
+  }
 
   await worker.terminate();
+
+  console.log(orc_text)
+
+  return ocr_text
 }
 
 changeColor.addEventListener("click", function() {
