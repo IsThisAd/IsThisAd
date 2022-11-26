db3aa30c05c1762ed417fe0c85250635571d4bd6
uyt8989 <kyungyun98@naver.com>
2022-11-26 16:51:09 +0900

    change order

--- popup.js
+++ popup.js
@@ -1,17 +1,6 @@
 let changeColor = document.getElementById("changeColor");
 
-// When the button is clicked, inject scaping.js/setColor
 
-function isLoading(option){
-  if(option){
-    changeColor.innerHTML = "<i class=\"fa fa-spinner fa-spin\">";
-    document.getElementById("message").innerHTML = "작업 처리중입니다...<br> 10초에서 20초 정도 소요될 수 있습니다."
-  }
-  else{
-    document.getElementById("message").innerHTML = "작업 처리 완료"
-    changeColor.innerHTML = " "
-  }
-}
 
 changeColor.addEventListener("click", async () => {
     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
@@ -29,3 +18,16 @@ changeColor.addEventListener("click", async () => {
       }
     });
   });
+
+// When the button is clicked, inject scaping.js/setColor
+
+function isLoading(option){
+  if(option){
+    changeColor.innerHTML = "<i class=\"fa fa-spinner fa-spin\">";
+    document.getElementById("message").innerHTML = "작업 처리중입니다...<br> 10초에서 20초 정도 소요될 수 있습니다."
+  }
+  else{
+    document.getElementById("message").innerHTML = "작업 처리 완료"
+    changeColor.innerHTML = " "
+  }
+}
