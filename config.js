3aa9b948150ed0b6d25a717382808b5cb0f4e124
uyt8989 <kyungyun98@naver.com>
2022-11-26 15:34:08 +0900

    config file

--- config.js
+++ config.js
@@ -1,1 +1,38 @@
-const Words = ["제공", "제공받아", "제공받", "원고료", "소정의", "소정", "업체", "협찬"]
+var config = {};
+
+config.ocr_keywords = [
+    "제공받", 
+    "원고료", 
+    "소정의", 
+    "소정", 
+    "업체", 
+    "협찬"
+];
+
+config.text_keywords = [
+    "제공받", 
+    "원고료", 
+    "소정의", 
+    "소정", 
+    "업체", 
+    "협찬"
+];
+
+config.ad_urls = [
+    "www.reve.net",
+    "xn--939au0g4vj8sq.net",
+    "www.mrblog.net",
+    "dinnerqueen.net",
+    "www.ringble.co.kr",
+    "www.seoulouba.co.kr",
+    "www.storyn.kr",
+    "reviewjin.com",
+    "mateb.kr",
+    "www.cometoplay.kr",
+    "enolja.com",
+    "meta-chehumdan.com",
+    "www.reviewplace.co.kr",
+    "reviewplace.co.kr",
+    "www.seoulouba.kr",
+    "seoulouba.co.kr"
+]
