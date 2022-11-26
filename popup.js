dc7ac40b10342da6ff9b8c7b9922689e8c4a4fdb
Ho-hoho <kennyya@naver.com>
2022-11-26 13:11:48 +0900

    changeBackgroundColor

--- popup.js
+++ popup.js
@@ -1,5 +1,21 @@
 let changeColor = document.getElementById("changeColor");
 
+var flag = false;
+function setColorNew(result) {
+    var urlRegex = /(https?:\/\/blog[^ "]*)/
+    document.querySelectorAll("li").forEach(item => {
+        var url = item.innerHTML.match(urlRegex)
+        url = item.innerHTML.match(urlRegex) ? item.innerHTML.match(urlRegex)[1] : ''
+        if (url.length > 0 && !url.includes("MyBlog")) {
+            flag = !flag;
+            if (flag)
+                item.style.cssText = "background-color: #ccffcc";
+            else
+                item.style.cssText = "background-color: #ffcccc"
+        }
+    })
+}
+
 // When the button is clicked, inject scaping.js/setColor
 changeColor.addEventListener("click", async () => {
     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
@@ -11,7 +27,12 @@ changeColor.addEventListener("click", async () => {
     }, (injectionResults) => {
       for (const frameResult of injectionResults){
         console.log(frameResult.result)
-        doOCR(frameResult.result)
+        result = doOCR(frameResult.result)
+        chrome.scripting.executeScript({
+          target: { tabId: tab.id },
+          func : setColorNew,
+          args : [result],
+        });
       }
     });
   });
