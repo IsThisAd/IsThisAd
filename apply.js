7b5bb57e8a64762bcabcc2c39e717a031b1624f5
JJHWAN <pp10202@naver.com>
2022-11-26 14:50:56 +0900

    Add config.js & apply.js

--- apply.js
+++ apply.js
@@ -1,6 +1,21 @@
 
+
+function findKeywords(results) {
+    console.log(results)
+
+    const labels = results.map((item) => (
+        item.data.text.includes("제공")
+    ));
+
+    console.log(labels)
+
+    return labels
+}
+
+
 function applyResults(ocrResult) {
 
+    findKeywords(ocrResult)
     var boolResult = []
     ocrResult.forEach(function(result) {
         result.forEach(function (string){
