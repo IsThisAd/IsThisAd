50c01018ccddb5a9b6dabc176e693a703d1b985c
uyt8989 <kyungyun98@naver.com>
2022-11-26 15:33:58 +0900

    apply.js

--- apply.js
+++ apply.js
@@ -1,25 +1,43 @@
+async function applyResults(ocrResults) {
+    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
+    console.log(ocrResults)
 
-function findKeywords(results) {
-    console.log(results)
-
-    const labels = results.map((item) => (
-        item.data.text.includes("제공")
-    ));
+    const texts = ocrResults.map((item) => removeWhitespace(item.data.text));
+    const labels = texts.map((item) => findKeywords(item))
 
     console.log(labels)
 
-    return labels
+    chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        func : setColorNew,
+        args : [labels],
+    })
 }
 
+function removeWhitespace(text) {
+    return text.replace(/ /g, '')
+}
 
-function applyResults(ocrResult) {
+function findKeywords(text) {
+    for (keyword of config.ocr_keywords) {
+        if (text.includes(keyword)) { return true }
+    }
+    return false
+}
 
-    findKeywords(ocrResult)
-    var boolResult = []
-    ocrResult.forEach(function(result) {
-        result.forEach(function (string){
-
-        })
+function setColorNew(labels) {
+    var index = 0
+    var urlRegex = /(https?:\/\/blog[^ "]*)/
+    document.querySelectorAll("li").forEach(item => {
+        var url = item.innerHTML.match(urlRegex)
+        url = item.innerHTML.match(urlRegex) ? item.innerHTML.match(urlRegex)[1] : ''
+        if (url.length > 0 && !url.includes("MyBlog")) {
+            if (!labels[index])
+                item.style.cssText = "background-color: #ccffcc";
+            else
+                item.style.cssText = "background-color: #ffcccc"
+            index++
+        }
     })
 }
